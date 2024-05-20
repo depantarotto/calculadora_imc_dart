1bd94793ddd751195e9081a2dafee5c8fab01226
Denis Pantarotto <depantarotto@gmail.com>
2024-05-20 13:13:53 -0300

    Commit incial.

---
+++ bin/calculadora_imc_dart.dart
@@ -0,0 +1,5 @@
+import 'package:calculadora_imc_dart/calculadora_imc_dart.dart' as calculadora_imc_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculadora_imc_dart.calculate()}!');
+}
